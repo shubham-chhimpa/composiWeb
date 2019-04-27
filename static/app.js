a4fe61fb8dc8b3421218ade1840d971a869ddc7f
Shubham <chhimpa.shubh04@gmail.com>
2019-04-27 15:59:23 +0530

    270420190311

--- static/app.js
+++ static/app.js
@@ -46,62 +46,62 @@
 //   });
 $("#loader").hide();
 
-
-function getTrack() {
- $("#index-banner").hide();
- $("#loader").show();
- var request = new XMLHttpRequest();   // new HttpRequest instance
- request.open("POST", "/gettrack", true);
- request.responseType = 'arraybuffer';
- request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
- request.onload = function (evt) {
-
-   var blob = new Blob([request.response], { type: 'audio/wav' });
-   var objectUrl = URL.createObjectURL(blob);
-   let aut = document.getElementById("sid");
-   aut.src = objectUrl;
-   document.getElementById("aid").load();
-   $("#loader").hide();
-   $("#index-banner").show();
-   console.log(evt);
-   console.log(blob);
- }
-
- let gnote = $("#input_text").val();
-
- console.log(gnote);
-
- request.send(JSON.stringify({ "notes": gnote }));
-}
-
-
-
-// function getTrack() {
-//   $("#index-banner").hide();
-//   $("#loader").show();
-//   var request = new XMLHttpRequest();   // new HttpRequest instance
-//   request.open("POST", "/gettrack", true);
-//   request.responseType = 'arraybuffer';
-//   request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-//   request.onload = function (evt) {
-
-//     var blob = new Blob([request.response], { type: 'audio/mpeg' });
-//     var objectUrl = URL.createObjectURL(blob);
-//     let aut = document.getElementById("sid");
-//     aut.src = objectUrl;
-//     document.getElementById("aid").load();
-//     $("#loader").hide();
-//     $("#index-banner").show();
-//     console.log(evt);
-//     console.log(blob);
-//   }
-
-//   let gnote = $("#input_text").val();
-
-//   console.log(gnote);
-
-//   request.send(JSON.stringify({ "notes": gnote }));
+//
+//function getTrack() {
+// $("#index-banner").hide();
+// $("#loader").show();
+// var request = new XMLHttpRequest();   // new HttpRequest instance
+// request.open("POST", "/gettrack", true);
+// request.responseType = 'arraybuffer';
+// request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+// request.onload = function (evt) {
+//
+//   var blob = new Blob([request.response], { type: 'audio/wav' });
+//   var objectUrl = URL.createObjectURL(blob);
+//   let aut = document.getElementById("sid");
+//   aut.src = objectUrl;
+//   document.getElementById("aid").load();
+//   $("#loader").hide();
+//   $("#index-banner").show();
+//   console.log(evt);
+//   console.log(blob);
 // }
+//
+// let gnote = $("#input_text").val();
+//
+// console.log(gnote);
+//
+// request.send(JSON.stringify({ "notes": gnote }));
+//}
+
+
+
+ function getTrack() {
+   $("#index-banner").hide();
+   $("#loader").show();
+   var request = new XMLHttpRequest();   // new HttpRequest instance
+   request.open("POST", "/gettrack", true);
+   request.responseType = 'arraybuffer';
+   request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+   request.onload = function (evt) {
+
+     var blob = new Blob([request.response], { type: 'audio/mpeg' });
+     var objectUrl = URL.createObjectURL(blob);
+     let aut = document.getElementById("sid");
+     aut.src = objectUrl;
+     document.getElementById("aid").load();
+     $("#loader").hide();
+     $("#index-banner").show();
+     console.log(evt);
+     console.log(blob);
+   }
+
+   let gnote = $("#input_text").val();
+
+   console.log(gnote);
+
+   request.send(JSON.stringify({ "notes": gnote }));
+ }
 
 
 getTrack();
